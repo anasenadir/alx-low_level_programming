b659fb33431afacb3fd1369e3af8344678b7e124
anasenadir <anasenadir2001@gmail.com>
2023-07-13 13:03:09 +0000

    update the 4-puts.c file. v-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
-* main - a simple program to print a message
+* main - a simple program to print a message with puts function 
 * returns - 0 (success)
 */
-int main(void){
+int main(void)
+{
  puts("\"Programming is like building a multilingual puzzle");
  return (0);
 }
