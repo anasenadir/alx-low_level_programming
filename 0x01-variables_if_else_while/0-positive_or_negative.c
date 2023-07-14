1ece43adf305cfa9134785bc34b1ec63021b5b0c
anasenadir <anasenadir2001@gmail.com>
2023-07-14 17:01:38 +0000

    create the first and second files

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+* main - A Simple Program To Check If A Rundom Number Is 
+* Grater Then, Less Then, Or Equal To Zero
+* Return Always 0. 
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 0 ){
+		printf("%d is positive\n" , n);
+	}else if(n == 0){
+		printf("%d is zero\n" , n);
+	}else{
+		printf("%d is negative\n" , n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	int last_digit_of_n = n % 10;
+
+	if(last_digit_of_n < 6 && last_digit_of_n != 0){
+		printf("Last digit of %d is %d and is less then 6 ans not 0" , n , last_digit_of_n);
+	}else if(last_digit_of_n  > 5 ){
+		printf("Last digit of %d is %d and is grater then 5" , n , last_digit_of_n);
+	}else if(last_digit_of_n == 0){
+		printf("Last digit of %d is %d and is 0" , n , last_digit_of_n);
+	}
+	return (0);
+}
