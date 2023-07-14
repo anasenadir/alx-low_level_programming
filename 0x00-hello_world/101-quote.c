0e2f67ed467d15c543097b33e21c1edef0d91008
anasenadir <anasenadir2001@gmail.com>
2023-07-14 02:45:36 +0000

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,9 @@
+/*
+ * File: 101-quote.c
+ */
+
 #include <unistd.h>
+
 /**
  * main - a simple program to print a message without using the printf or puts
  *
@@ -6,7 +11,7 @@
  */
 int main(void)
 {	
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    	write(2, message, sizeof(message));
-    	return 1;
+    	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+    	return (1);
 }
