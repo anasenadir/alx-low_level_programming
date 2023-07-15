a774b553e6580fef45b5b99126517255c279628b
anasenadir <anasenadir2001@gmail.com>
2023-07-15 02:56:38 +0000

    update the file number 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,8 @@
  */
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
-	{
-		printf("%d", i);
-	}
-	printf('\n');
+	for (int n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
