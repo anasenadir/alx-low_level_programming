7eb0f5a949e0e19d9c34a1a2013584b0c1bb3469
Anas En-nadir <anasenadir2001@gmail.com>
2023-07-19 02:46:29 +0100

    prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <main.h>
 
 /**
 * main - prints _putchar, followed by a new line.
+* Desc: print '_putchar' statement
 * Return: Always 0
 */
 int main(void)
 {
-	printf("_putchar");
+	char str[] = "_putchar";
+	int ch = 0;
+
+	for (ch; ch < 8; ch++)
+	{
+		_putchar(str[ch]);
+	}
 	return (0);
 }
