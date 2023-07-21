6a9570858aac1e499301444d294ce1830e31943c
Anas En-nadir <anasenadir2001@gmail.com>
2023-07-21 18:50:14 +0100

    modify the "5-more_numbers" function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,10 @@ void more_numbers(void)
     {
         for (j = 0 ; j <= 14; j++)
         {
-            if(j > 9){
+            if(j > 9)
                 _putchar(j / 10 + '0');
-                _putchar(j % 10 + '0');
-                continue;
-            }
             _putchar(j + '0');
         }
         _putchar('\n');
     }
-    _putchar('\n');
 }
