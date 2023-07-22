b4b8bed9631097bdeeb3c2e95331cf51c726af85
Anas En-nadir <anasenadir2001@gmail.com>
2023-07-22 02:22:44 +0100

    fix some other error in  fizz_buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,10 +16,10 @@ int main(void)
         if (i % 3 == 0 && i % 5 == 0){
             printf("FizzBuzz");
         }
-        else if (i % 3 == 0){
+        else if (i % 3 == 0 && i % 5 != 0){
             printf("Fizz");
         }
-        else if (i % 5 == 0){
+        else if (i % 3 != 0 && i % 5 == 0){
             printf("Buzz");
         }
         else{
