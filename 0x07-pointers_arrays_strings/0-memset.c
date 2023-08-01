e16be822e4b9b29e0fbf7fbf36e595bd1e78091a
Anas En-nadir <anasenadir2001@gmail.com>
2023-07-31 18:09:58 -0700

    update the 0-memset and main.h files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		*(s + i) = b;
+		i++;
 	}
 
 	return (s);
