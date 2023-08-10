e7ef6ee70b894896d84077abc12ec56bc63019e8
Anas En-nadir <anasenadir2001@gmail.com>
2023-08-09 19:09:35 -0700

    fix the second bug

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-
-/**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-	int count = 0;
-
-	while (i != 0)
-	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-		else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
 
 /**
  * main - Takes in exactly one argument for minimum coin count
@@ -47,25 +10,26 @@ int coinConverter(int i)
  */
 int main(int argc, char *argv[])
 {
-	int i, coin;
+	if (argc == 2)
+	{
+		int i, least = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
 
-	coin = 0;
-
-	if (argc != 2)
+		for (i = 0; i < 25; i++)
+		{
+			if (money >= cents[i])
+			{
+				least += money / cents[i];
+				if (money % cents[i] == 0)
+					break;
+			}
+		}
+		printf("%d\n", least);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
-	}
-
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-	else
-	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
 	}
 
 	return (0);
