4793a8402322b91a60340c0ec9ccc6290a78dfa1
Anas En-nadir <anasenadir2001@gmail.com>
2023-08-09 18:47:18 -0700

    create the project file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc - 1);
+	return (0);
+}
