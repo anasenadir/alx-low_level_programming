4ca36f8e80a8ade74da03fdd694dacd74ecf2079
Anas En-nadir <anasenadir2001@gmail.com>
2023-08-22 11:09:49 -0700

    create the project files

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters.
+ * @...: A variable number of paramters.
+ *
+ * Return: If n == 0 - returns 0.
+ *         Otherwise - returns the sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, sum = 0;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	char *str;
+	unsigned int index;
+
+	va_start(args, n);
+
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
