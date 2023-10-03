29733423861e999d93be77cd87e02b709e1d49ee
anasen <anasenadir2001@gmail.com>
2023-10-02 17:25:57 -0700

    fix some errors prt-1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -15,5 +15,6 @@
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,5 +12,6 @@
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
+	(void)argv;
 	return (0);
 }
