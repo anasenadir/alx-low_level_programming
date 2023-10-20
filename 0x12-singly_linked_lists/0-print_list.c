2e20c3892c96ce2676086ffab041218c75368e00
root <root@DESKTOP-PDMJJRJ.localdomain>
2023-10-20 03:51:16 +0200

    try another algo to get 100% for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,35 @@
 #include "lists.h"
 
 /**
+* _strlen - return the length of an array
+* @str: the str pointer
+* Return: return the length
+*/
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
 * print_list - function that prints all the elements of a list_t list.
 * @h: a struct poiter pointes to the head of the linked list
 * Return: the number of nodes
 */
 size_t print_list(const list_t *h)
 {
-	const list_t *head_ref = h;
 	size_t len = 0;
 
 
-	while (head_ref)
+	while (h)
 	{
-		if (head_ref->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", head_ref->len, head_ref->str);
 
-		head_ref = head_ref->next;
+		printf("[%u] %s\n", h->len, h->str ? h->str : "(nil)");
+		h = h->next;
 		len++;
 	}
 
