1ba2e8575b9f1acddf7c50a3c2a0d1ea5e4d5630
root <root@DESKTOP-UFUOI7S>
2023-10-24 23:31:16 +0100

    fix the file 90insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *new_node = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
-	if (!new_node || !node)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->n = n;
