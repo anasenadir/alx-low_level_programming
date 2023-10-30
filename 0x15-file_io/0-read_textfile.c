b02fc5498b585f121396df7bbb25f9e87438c8e4
root <root@DESKTOP-BJGLETJ>
2023-10-30 16:46:30 -0700

    function that reads a text file and prints it to the POSIX standard output.4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,24 +12,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fptr;
 	char buffer[BUFFER_SIZE * 8];
-	ssize_t readed_bytes, wrote_bytes;
+	ssize_t bytes;
 
 	if (!filename || !letters)
 		return (0);
-	
-	fptr = open(filename, O_RDONLY);
 
+	fptr = open(filename, O_RDONLY);
 	if (fptr == -1)
 		return (0);
 
-	readed_bytes = read(fptr, buffer, letters);
-	wrote_bytes = write(STDOUT_FILENO, buffer, readed_bytes);
+	bytes = read(fptr, buffer, letters);
+	bytes = write(STDOUT_FILENO, buffer, bytes);
 
-	if (wrote_bytes < 0 || wrote_bytes != readed_bytes)
-	{
-		close(fptr);
-		return (0);
-	}
 	close(fptr);
-	return (readed_bytes);
+	return (bytes);
 }
