21c06856683d3dc40477de927065d8a55c37199f
root <root@DESKTOP-PDMJJRJ.localdomain>
2023-12-23 03:17:26 +0100

    fix betty style

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,13 +9,13 @@
  *         be stored in the array of the hash table
 */
 unsigned long int key_index(const unsigned char *key,
-                            unsigned long int size)
+							unsigned long int size)
 {
-    unsigned long int index;
+	unsigned long int index;
 
-    if (!size || !key)
-        return (0);
+	if (!size || !key)
+		return (0);
 
-    index = hash_djb2(key) % size;
-    return (index);
+	index = hash_djb2(key) % size;
+	return (index);
 }
