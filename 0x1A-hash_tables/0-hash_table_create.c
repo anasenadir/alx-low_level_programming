bbe4963c1c38869fab24f802d91e9481f3c67b5c
root <root@DESKTOP-PDMJJRJ.localdomain>
2023-12-23 03:13:13 +0100

    fix the hashtable create function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht;
 	unsigned long int i;
-	hash_table_t *table;
 
-	table = malloc(sizeof(hash_table_t));
-
-	if (!table)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (!table->array)
-	{
-		free(table);
+
+	if (ht->array == NULL)
 		return (NULL);
-	}
-
-	i = 0;
-	while (i < size)
-		table->array[i++] = NULL;
-
-	return (table);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
 }
